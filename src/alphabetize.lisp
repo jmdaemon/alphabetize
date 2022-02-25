a1a378df3aa79c29402c4805d216e168b2fabc43
Joseph Diza <josephm.diza@gmail.com>
2022-02-24 18:24:45 -0800

    Add initial alphabetize functions

--- src/alphabetize.lisp
+++ src/alphabetize.lisp
@@ -3,6 +3,29 @@
   (:documentation "Alphabetizes lists or characters")
   (:export :main))
 (in-package :alphabetize)
+(require :for)
+
+(defparameter strings (list "#include <pcre.h>"
+                       "#include <stdlib.h>"
+                       "#include <string.h>"
+                       "#include <math.h>"
+                       "#include <argp.h>"
+                       "#include <stdio.h>"
+                       "#include <stdbool.h>"))
+
+(defun alphabetize(strings)
+  "Lexicographically alphabetizes a list of strings"
+  (sort strings #'sb-unicode:unicode<))
+
+;(defparameter sorted (sort strings #'sb-unicode:unicode<))
+(defparameter sorted (alphabetize strings))
+
+(defun display(strings)
+  "Shows the sorted list of strings delimited by new lines"
+  (for:for ((str over strings))
+  (format t "~a~%" str)))
+
+(display sorted)
 
 (defun main ()
   (print "Start of Main"))
